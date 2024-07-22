e24b4deed2d185fd120e3372d65fb285437dda85
esthermurambikar <anupriyamurambikar@gmail.com>
2024-07-22 23:00:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,5 +14,5 @@ def submit():
 
 
 if __name__ == "__main__":
-	app.run(debug=True)
-	app.run(host = '0.0.0.0', port =9000 , debug=True) 
+	app.debug=True
+	app.(host = '0.0.0.0', port =9000 , debug=True) 
